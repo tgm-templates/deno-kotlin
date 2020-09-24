d571df465a7ecc4e2f14d423de760bf1695a0011
linux_china <libing.chen@gmail.com>
2020-09-24 00:56:00 -0700

    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+  kotlin("js") version "1.4.10"
+  id("deno-plugin")
+}
+
+group = "@group@"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.9")
+  testImplementation(kotlin("test-js"))
+}
+
+kotlin {
+  //js(IR)
+  js {
+    useCommonJs()
+    binaries.executable()
+    nodejs()
+  }
+}
+
+tasks {
+  //task from buildSrc
+  register<org.mvnsearch.DenoTask>("deno") {
+    dependsOn("mainClasses")
+    group = "Deno"
+    description = "Build Deno file from Kotlin"
+  }
+}
