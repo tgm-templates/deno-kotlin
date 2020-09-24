8bb1129f91949336e45f53b94be057ddb8f9b06b
linux_china <libing.chen@gmail.com>
2020-09-24 00:47:28 -0700

    UserManagerTest

---
+++ src/test/kotlin/user/UserManagerTest.kt
@@ -0,0 +1,13 @@
+package user
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class UserManagerTest {
+  @Test
+  fun testBar() {
+    console.log("Hello World!")
+    assertEquals(4, 2 * 2)
+  }
+
+}
