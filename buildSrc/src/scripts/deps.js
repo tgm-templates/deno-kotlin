2f89a51dbb27f02e732e88a1725159d57faffe13
Libing Chen <libing.chen@gmail.com>
2021-02-24 14:39:35 -0800

    Update to 0.88.0

--- buildSrc/src/scripts/deps.js
+++ buildSrc/src/scripts/deps.js
@@ -1,4 +1,2 @@
 // noinspection ES6UnusedImports
-import {encode, decode} from "https://deno.land/std@0.74.0/encoding/utf8.ts"
-// noinspection ES6UnusedImports
-import {createHash} from "https://deno.land/std@0.74.0/hash/mod.ts";
+import {createHash} from "https://deno.land/std@0.88.0/hash/mod.ts";
