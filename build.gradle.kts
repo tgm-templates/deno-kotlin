c54743bf54293c1f197325f903adb9ad19c011f0
Libing Chen <libing.chen@gmail.com>
2021-03-04 00:26:44 -0800

    Update to  kotlinx-coroutines 1.4.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.4.2")
+  implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.4.3")
   testImplementation(kotlin("test-js"))
 }
 
