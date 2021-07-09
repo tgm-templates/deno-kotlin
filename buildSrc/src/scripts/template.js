c99427bfb9d1ba2c108a39fed2ee8cc8f5487247
Libing Chen <libing.chen@gmail.com>
2021-07-09 14:27:14 -0700

    Update to kotlin 1.5.20 & coroutines 1.5.1

--- buildSrc/src/scripts/template.js
+++ buildSrc/src/scripts/template.js
@@ -1,5 +1,5 @@
-import Kotlin from "https://jspm.dev/kotlin@1.5.0"
-import KotlinxCoroutinesCore from "https://jspm.dev/kotlinx-coroutines-core@1.5.0"
+import Kotlin from "https://jspm.dev/kotlin@1.5.20"
+import KotlinxCoroutinesCore from "https://jspm.dev/kotlinx-coroutines-core@1.5.1"
 
 //===== global imports ===========
 //deps.js
